bab6cb796c9d7e5f020b19edeab636ddb54a4b28
mtalha4t7 <83336467+mtalha4t7@users.noreply.github.com>
2024-10-21 20:06:48 +0500

    mon,21 8:06

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cura_link/screens/patient/patient_main.dart';
 import 'package:cura_link/splashScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
