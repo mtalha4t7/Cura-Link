c27a6b3ff6ab501612116a0f4994d3dbd71c371c
mtalha4t7 <27708@students.riphah.edu.pk>
2024-11-20 02:02:40 +0500

    updated server

--- lib/src/screens/features/authentication/controllers/signup_controller.dart
+++ lib/src/screens/features/authentication/controllers/signup_controller.dart
@@ -1,69 +1,69 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import '../../../../repository/authentication_repository/authentication_repository.dart';
-import '../../../../repository/user_repository/user_repository.dart';
-import '../../../../shared prefrences/shared_prefrence.dart';
-import '../models/user_model.dart';
+  import 'package:flutter/material.dart';
+  import 'package:get/get.dart';
+  import '../../../../repository/authentication_repository/authentication_repository.dart';
+  import '../../../../repository/user_repository/user_repository.dart';
+  import '../../../../shared prefrences/shared_prefrence.dart';
+  import '../models/user_model.dart';
 
-class SignUpController extends GetxController {
-  static SignUpController get instance => Get.find();
+  class SignUpController extends GetxController {
+    static SignUpController get instance => Get.find();
 
-  final showPassword = false.obs;
-  final isGoogleLoading = false.obs;
-  final isFacebookLoading = false.obs;
-  GlobalKey<FormState> signupFormKey = GlobalKey<FormState>();
+    final showPassword = false.obs;
+    final isGoogleLoading = false.obs;
+    final isFacebookLoading = false.obs;
+    GlobalKey<FormState> signupFormKey = GlobalKey<FormState>();
 
-  // TextField Controllers to get data from TextFields
-  final email = TextEditingController();
-  final password = TextEditingController();
-  final fullName = TextEditingController();
-  final phoneNo = TextEditingController();
+    // TextField Controllers to get data from TextFields
+    final email = TextEditingController();
+    final password = TextEditingController();
+    final fullName = TextEditingController();
+    final phoneNo = TextEditingController();
 
-  /// Loader
-  final isLoading = false.obs;
+    /// Loader
+    final isLoading = false.obs;
 
-  Future<void> createUser() async {
-    try {
-      isLoading.value = true;
-      if (!signupFormKey.currentState!.validate()) {
+    Future<void> createUser() async {
+      try {
+        isLoading.value = true;
+        if (!signupFormKey.currentState!.validate()) {
+          isLoading.value = false;
+          return;
+        }
+
+        // Load user type from shared preferences
+        String? userType = await loadUserType();
+        if (userType == null) {
+          throw "User type is not set.";
+        }
+
+        // Create user model
+        final user = UserModel(
+          email: email.text.trim(),
+          password: password.text.trim(),
+          fullName: fullName.text.trim(),
+          phoneNo: phoneNo.text.trim(),
+          userType: userType,
+        );
+
+        // Authenticate user and create in repository
+        final auth = AuthenticationRepository.instance;
+        await auth.registerWithEmailAndPassword(user.email, user.password!);
+        await UserRepository.instance.mongoCreateUser(user);
+        await UserRepository.instance.createUser(user);
+        auth.setInitialScreen(auth.firebaseUser);
+      } catch (e) {
         isLoading.value = false;
-        return;
+        Get.snackbar("Error", e.toString(),
+            snackPosition: SnackPosition.BOTTOM,
+            duration: const Duration(seconds: 5));
       }
+    }
 
-      // Load user type from shared preferences
-      String? userType = await loadUserType();
-      if (userType == null) {
-        throw "User type is not set.";
+    Future<void> phoneAuthentication(String phoneNo) async {
+      try {
+        await AuthenticationRepository.instance.phoneAuthentication(phoneNo);
+      } catch (e) {
+        throw e.toString();
       }
-
-      // Create user model
-      final user = UserModel(
-        email: email.text.trim(),
-        password: password.text.trim(),
-        fullName: fullName.text.trim(),
-        phoneNo: phoneNo.text.trim(),
-        userType: userType,
-      );
-
-      // Authenticate user and create in repository
-      final auth = AuthenticationRepository.instance;
-      await auth.registerWithEmailAndPassword(user.email, user.password!);
-      await UserRepository.instance.mongoCreateUser(user);
-      await UserRepository.instance.createUser(user);
-      auth.setInitialScreen(auth.firebaseUser);
-    } catch (e) {
-      isLoading.value = false;
-      Get.snackbar("Error", e.toString(),
-          snackPosition: SnackPosition.BOTTOM,
-          duration: const Duration(seconds: 5));
     }
   }
-
-  Future<void> phoneAuthentication(String phoneNo) async {
-    try {
-      await AuthenticationRepository.instance.phoneAuthentication(phoneNo);
-    } catch (e) {
-      throw e.toString();
-    }
-  }
-}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 const DB =
     "mongodb+srv://25362:talha8k83t@curalinkcluster.0xafs.mongodb.net/dbCuraLink?retryWrites=true&w=majority&appName=CuraLinkCluster";
 const COLLECTION_NAME = "users";
 
-mongoose.connect(DS).then(() => {
+mongoose.connect(DB).then(() => {
   console.log("Connection successful");
 }).catch((e)=> {
   console.log(e);
