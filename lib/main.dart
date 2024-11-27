3ce63890397976f6d49b42a888828a930857e318
mtalha4t7 <83336467+mtalha4t7@users.noreply.github.com>
2024-11-27 05:39:21 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-import 'package:cura_link/src/mongodb/mongodb.dart';
-import 'package:cura_link/src/repository/authentication_repository/authentication_repository.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'app.dart';
 import 'firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:cura_link/src/mongodb/mongodb.dart';
+import 'package:cura_link/src/repository/authentication_repository/authentication_repository.dart';
 
 Future<void> main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -16,12 +16,31 @@ Future<void> main() async {
 
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
+  // Initialize MongoDB
   await MongoDatabase.connect();
 
+  // Initialize Firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
       .then((_) => Get.put(AuthenticationRepository()));
-  FirebaseAppCheck appCheck = FirebaseAppCheck.instance;
 
-  await appCheck.activate(androidProvider: AndroidProvider.playIntegrity);
+  // Initialize Firebase App Check
+  await FirebaseAppCheck.instance.activate(
+    // You can also use a `ReCaptchaEnterpriseProvider` provider instance as an
+    // argument for `webProvider`
+    webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+    // Default provider for Android is the Play Integrity provider. You can use the "AndroidProvider" enum to choose
+    // your preferred provider. Choose from:
+    // 1. Debug provider
+    // 2. Safety Net provider
+    // 3. Play Integrity provider
+    androidProvider: AndroidProvider.playIntegrity,
+    // Default provider for iOS/macOS is the Device Check provider. You can use the "AppleProvider" enum to choose
+    // your preferred provider. Choose from:
+    // 1. Debug provider
+    // 2. Device Check provider
+    // 3. App Attest provider
+    // 4. App Attest provider with fallback to Device Check provider (App Attest provider is only available on iOS 14.0+, macOS 14.0+)
+    appleProvider: AppleProvider.appAttest,
+  );
   runApp(const App());
 }
